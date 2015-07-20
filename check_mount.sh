1d12874d60de419ff5f394265d14aa1b35a0b347
蓝枫 <ibuler@qq.com>
2015-07-20 16:24:58 +0800

    Update check_mount.sh
    update

--- check_mount.sh
+++ check_mount.sh
@@ -6,14 +6,27 @@
 cwdir=`dirname $0`
 . $cwdir/function.sh
 
-check_mount() {
-    mount | awk '{ print $1, $3 }' | grep $1 | grep $2 &> /dev/null && echo 1 || echo 0
+mount_discovery() {
+    partition=($(grep -v '^#' $check_list | grep "^mount:" | awk '{ print $2 }'))
+    discovery ${partition[@]}
 }
 
+check_mount() {
+    target_dir=$(grep -v '^#' $check_list | grep "^mount:" | grep $1 | awk '{ print $3}' )
+    mount | awk '{ print $1, $3 }' | grep $1 | grep $target_dir &> /dev/null && echo 1 || echo 0
+}
 
-if [ -z "$1"  -o -z "$2" -o "$1" == '-h' ];then
-    msg="Usage: $0 dev target_dir"
-    echo $msg
-else
-    check_mount $1 $2
-fi
+case $1 in
+discovery)
+    mount_discovery;;
+*)
+    if [ -z "$1"  -o  "$1" == '-h' ];then
+        echo $1
+        msg="Usage: $0 discovery | dev"
+        echo $msg
+    else
+        check_mount $1
+    fi
+    ;;
+
+esac
